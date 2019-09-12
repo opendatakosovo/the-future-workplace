dac6d0506ccde067eacfc773685269b50d7627c4
the3reze <ereze.ahmetgjekaj@gmail.com>
2019-09-12 15:26:49 +0200

    Maximize Button and Lang

--- resources/views/partials/top.blade.php
+++ resources/views/partials/top.blade.php
@@ -22,7 +22,11 @@
             <div class="collapse navbar-collapse" id="navbar-mobile">
                 <ul class="nav navbar-nav mr-auto float-left">
                     <li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu"></i></a></li>
-                    <li class="nav-item d-none d-md-block"><a class="nav-link nav-link-expand" href="#"><i class="ficon ft-maximize"></i></a></li>
+                    <li class="nav-item d-none d-md-block">
+                        <a class="nav-link nav-link-expand" href="#">
+                            <i class="ficon ft-maximize"></i>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
