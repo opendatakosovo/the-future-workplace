4e30ef170f1c92b94dffc593cf9ae0fa238da062
Dren <dren.mullafetahu1@gmail.com>
2019-09-16 15:08:53 +0200

    landing page with video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,109 @@
 @extends('layouts/main')
 @section('title', 'Home')
 @section('content')
-    <div class="col-12 d-flex align-items-center justify-content-center">
-        <div class="col-lg-4 col-md-6 col-10 box-shadow-2 p-0">
-            <div class="card border-grey border-lighten-3 px-1 py-1 box-shadow-3 m-0">
-                <div class="card-header border-0">
-                    <div class="text-center mb-1">
-                        <img src="app-assets/images/logo/logo.png" alt="branding logo">
-                    </div>
-                    <div class="font-large-1  text-center">
-                        This page is under maintenance
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('/app-assets/css/landing/linearicons.css')}}">
+    <!-- Start Banner Area -->
+    <section class="home-banner-area relative" style="width: 100%">
+        <div class="container">
+            <div class="row fullscreen d-flex align-items-center justify-content-center">
+                <div class="banner-content col-lg-8 col-md-12" style="position: absolute; margin-top: -300px">
+                    <img class="brand-logo" alt="creaative admin logo" src="app-assets/images/logo/logo.svg" style="width: 7%;box-shadow: blue;">
+                    <h1 class="wow fadeIn" data-wow-duration="4s">The Future Workplace</h1>
+                    <p class="text-white">
+                        Choose your Future Career wisely!
+                    </p>
+                </div>
+                <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" >
+                    <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
+    </section>
+
+    <!-- End Banner Area -->
+    <section class="feature-area" style="width: 100%">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="section-title text-center">
+                        <h1 style="margin-top: 3%">Features That Make Us Hero</h1>
+                        <p>
+                            If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for
+                            as low as $.17 each.
+                        </p>
                     </div>
                 </div>
-                <div class="card-body text-center pt-0">
-                    <p>We're sorry for the inconvenience.
-                        <br> Please check back later.</p>
-                    <div class="mt-2">
-                        <i class="la la-cog spinner font-large-2 danger"></i>
+            </div>
+            <div class="feature-inner row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature-item">
+                        <i class="ti-crown"></i>
+                        <h4>Architecture</h4>
+                        <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay=".1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">
+                            <p>
+                                Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <hr>
-                <div class="text-center pt-2 pb-2">
-                    <a href="#" class="btn btn-social-icon round mr-1 mb-1 btn-facebook">
-                        <span class="ft-facebook"></span>
-                    </a>
-                    <a href="#" class="btn btn-social-icon round mr-1 mb-1 btn-twitter">
-                        <span class="ft-twitter"></span>
-                    </a>
-                    <a href="#" class="btn btn-social-icon round mr-1 mb-1 btn-instagram">
-                        <span class="ft-instagram"></span>
-                    </a>
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature-item">
+                        <i class="ti-briefcase"></i>
+                        <h4>Interior Design</h4>
+                        <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay=".3s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeIn;">
+                            <p>
+                                Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature-item">
+                        <i class="ti-medall-alt"></i>
+                        <h4>Concept Design</h4>
+                        <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay=".5s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.5s; animation-name: fadeIn;">
+                            <p>
+                                Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature-item">
+                        <i class="ti-key"></i>
+                        <h4>Lifetime Access</h4>
+                        <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay=".1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">
+                            <p>
+                                Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature-item">
+                        <i class="ti-files"></i>
+                        <h4>Source File Included</h4>
+                        <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay=".3s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeIn;">
+                            <p>
+                                Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature-item">
+                        <i class="fas fa-fish"></i>
+                        <h4>Live Support</h4>
+                        <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay=".5s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.5s; animation-name: fadeIn;">
+                            <p>
+                                Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
 @endsection
