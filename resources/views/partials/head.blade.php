d84ca76da5def3741e69d0aa87b8f4563d83b89e
Dren <dren.mullafetahu1@gmail.com>
2019-09-18 16:53:33 +0200

    Update head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,10 +3,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 <meta name="description"
       content="The Future Workplace is a project to help increase ICT market information in Kosovo in order to contribute to employment opportunities through among other identifying services offered in Kosovo and business opportunities.">
-<meta name="keywords" content="Home,Statistics,Company Data,Workf orce Data,Skills Mismatch,Blog,About,Download">
+<meta name="keywords" content="Home,Statistics,Company Data,Workforce Data,Skills Mismatch,Blog,About,Download">
 <meta name="author" content="Open Data Kosovo">
 <title>@yield('title', 'The Future Workplace')</title>
-<link rel="apple-touch-icon" href="{{URL::asset('/app-assets/images/ico/apple-icon-120.png')}}">
+<link rel="apple-touch-icon" href="{{URL::asset('/app-assets/images/logo/logo.png')}}">
 <link rel="shortcut icon" type="image/x-icon" href="{{URL::asset('/app-assets/images/logo/logo.png')}}">
 <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700"
       rel="stylesheet">
