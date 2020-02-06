3950ff97664ee67c801cdeef88154ba8ce8f18a1
the3reze <ereze.ahmetgjekaj@gmail.com>
2020-02-06 16:11:49 +0100

    Update stats.blade.php

--- resources/views/client/stats.blade.php
+++ resources/views/client/stats.blade.php
@@ -24,7 +24,7 @@
                         <div class="heading-elements">
                             <ul class="list-inline mb-0">
                                 <li>
-                                    <a class="btn btn-glow btn-round btn-bg-gradient-x-green-pink" href="{{ url('workforce-map') }}">{{Lang::get('translation.more')}}</a>
+                                    <a class="btn btn-glow btn-round btn-bg-gradient-x-green-pink" href="{{URL::route('workforce-map')}}">{{Lang::get('translation.more')}}</a>
                             </ul>
                         </div>
                     </div>
