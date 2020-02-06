7142ba1fe3a4efb450ba6a35618094b488034a67
the3reze <ereze.ahmetgjekaj@gmail.com>
2020-02-06 14:04:07 +0100

    Head Title in Admin changed

--- resources/views/partials/admin/head.blade.php
+++ resources/views/partials/admin/head.blade.php
@@ -5,7 +5,7 @@
 <meta name="keywords" content="admin template, Chameleon admin template, dashboard template, gradient admin template, responsive admin template, webapp, eCommerce dashboard, analytic dashboard">
 <meta name="author" content="ThemeSelect">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<title>Dashboard eCommerce - Chameleon Admin - Modern Bootstrap 4 WebApp & Dashboard HTML Template + UI Kit</title>
+<title>The Future Workplace</title>
 <link rel="apple-touch-icon" href="/admin/app-assets/images/ico/apple-icon-120.png">
 <link rel="shortcut icon" type="image/x-icon" href="/app-assets/images/ico/favicon.ico">
 <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
